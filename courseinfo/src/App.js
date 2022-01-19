374a192ac0cdf80f2ab73a14e53dcfe2ddcd28db
Asim Shrestha <asimshrestha608@gmail.com>
2022-01-19 16:20:56 +0545

    courseinfo: complete step5(1.5)

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -31,27 +31,29 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundaments of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass the data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundaments of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass the data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
